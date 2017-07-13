a317ead38ffe4b618167a53249b2ab0f19173e12
Taylor Smith <jtsmith2@gmail.com>
2017-07-13 09:08:39 -0400

    Create settings.py

---
+++ settings.py
@@ -0,0 +1,11 @@
+#This file needs to be filled out for ynabemail.py to work.
+
+ynab_user = ''         # Your username on YNAB Example: someone@domain.com
+ynab_password = ''     # Your password for YNAB
+ynab_budgetname = ''   # Example:  My Budget
+
+gmail_user = ''        # Your Gmail username
+gmail_password = ''    # Your Gmail password. If you have 2-factor auth setup, create an 'app password'
+                       # See https://support.google.com/accounts/answer/185833?hl=en
+to_list = ['']         # A list of emails to send the email to. Example: ['email@domain.com','email2@otherdomain.com']
+from_address = ''      # The gmail address the email is to be from
